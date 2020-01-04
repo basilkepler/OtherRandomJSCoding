e7268918d7211547a6f5f30959f2bdc2f168ba34
basilkepler <59282970+basilkepler@users.noreply.github.com>
2020-01-04 12:34:23 +0200

    save

---
+++ RootFolder/0005/app.js
@@ -0,0 +1,12 @@
+function xyz(in_arr1,in_arr2,in_target){
+n = 0
+for(i=0;i<in_arr1.length;i++){
+  for(j=0;j<in_arr2.length;j++){
+    
+  }  
+}
+}
+arr1 = [-1,3,8,2,9,5]
+arr2 = [4,1,2,10,5,20]
+target = 24
+xyz(arr1,arr2,target)
---
+++ RootFolder/0005/app2.js
@@ -0,0 +1,25 @@
+let varOne = 0;
+for (i = 0; i < 4; i++){
+for (j = 0; j < 4; j++){
+varOne++;
+}
+}
+console.log(varOne);
+
+
+/* store_obj = []
+for(i=0;i < in_arr1.length;i++){
+    for(j=0;j < in_arr2.length;j++){
+        store_obj[i][j] = {
+            x: i,
+            y: j,
+            arr: Math.abs(in_arr1[i] + in_arr2[j] - 24)
+        }
+        console.log('x:', x)
+        console.log('y:', y)
+        console.log('in_arr1:', in_arr1)
+        console.log('in_arr2:', in_arr2)
+        console.log('arr:', arr)
+    }
+        
+    } */
